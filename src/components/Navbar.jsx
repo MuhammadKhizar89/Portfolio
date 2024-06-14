6529b748113bacef86c2a61a3dc78d8ff5c24f7b
MuhammadKhizar89 <muhammadkhizar8919@gmail.com>
2024-06-14 21:13:24 +0500

    Hero Created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import { AiOutlineClose,AiOutlineMenu } from 'react-icons/ai'
+const Navbar = () => {
+    const [nav,setNav]=useState(false);
+    const handleNav=()=>{
+        setNav(!nav);
+    }
+  return (
+    <div className='text-gray-500 flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 ' >
+        <h1 className=' text-3xl font-bold primary-color ml-4' >M.Khizar</h1>
+        <ul className='hidden md:flex' >
+            <li className='p-2' >Home</li>
+            <li className='p-2' >Company</li>
+            <li className='p-2' >Resources</li>
+            <li className='p-2' >About</li>
+            <li className='p-2' >Contact</li>
+        </ul>
+
+        <div onClick={handleNav} className='block md:hidden' >
+        {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
+        </div>
+
+        <div className={nav?' text-gray-300 fixed h-full left-0 top-0 w-[60%] border-r border-r-gray-900 bg-[#202121] ease-in-out duration-500 ':'fixed left-[-100%]'}>
+        <h1 className=' text-3xl m-4  font-bold primary-color ml-4' >M.Khizar</h1>
+                <ul className='p-4' >
+                    <li className='p-2' >Home</li>
+                    <li className='p-2' >Company</li>
+                    <li className='p-2' >Resources</li>
+                    <li className='p-2' >About</li>
+                    <li className='p-2' >Contact</li>
+                </ul>
+
+        </div>
+
+        </div>
+  )
+}
+
+export default Navbar
