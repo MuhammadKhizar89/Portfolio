f953247b147a6f6c32c5a198894d58a3a2bbe2d9
MuhammadKhizar89 <muhammadkhizar8919@gmail.com>
2024-06-15 17:27:20 +0500

    Navbar Scroll added 3.0

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+import { Link } from 'react-scroll';
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -25,11 +26,11 @@ const Navbar = () => {
     <div className='text-gray-500 flex justify-between items-center h-24 w-full px-4'>
       <h1 className='text-3xl font-bold primary-color ml-4'>M.Khizar</h1>
       <ul className='hidden md:flex'>
-        <li className='p-5'><a href="#Home">Home</a></li>
-        <li className='p-5'><a href="#AboutMe">AboutMe</a></li>
-        <li className='p-5'><a href="#Projects">Projects</a></li>
-        <li className='p-5'><a href="#Experience">Experience</a></li>
-        <li className='p-5'><a href="#Contact">Contact</a></li>
+        <li className='p-5'><Link to="Home" smooth={true} duration={500}>Home</Link></li>
+        <li className='p-5'><Link to="AboutMe" smooth={true} duration={500}>AboutMe</Link></li>
+        <li className='p-5'><Link to="Projects" smooth={true} duration={500}>Projects</Link></li>
+        <li className='p-5'><Link to="Experience" smooth={true} duration={500}>Experience</Link></li>
+        <li className='p-5'><Link to="Contact" smooth={true} duration={500}>Contact</Link></li>
       </ul>
 
       <div onClick={handleNav} className='block md:hidden'>
@@ -42,11 +43,11 @@ const Navbar = () => {
       >
         <h1 className='text-3xl m-4 font-bold primary-color ml-4'>M.Khizar</h1>
         <ul className='p-4' onClick={handleNav}>
-          <li className='p-2'><a href="#Home">Home</a></li>
-          <li className='p-2'><a href="#AboutMe">AboutMe</a></li>
-          <li className='p-2'><a href="#Projects">Projects</a></li>
-          <li className='p-2'><a href="#Experience">Experience</a></li>
-          <li className='p-2'><a href="#Contact">Contact</a></li>
+          <li className='p-2'><Link to="Home" smooth={true} duration={500} onClick={handleNav}>Home</Link></li>
+          <li className='p-2'><Link to="AboutMe" smooth={true} duration={500} onClick={handleNav}>AboutMe</Link></li>
+          <li className='p-2'><Link to="Projects" smooth={true} duration={500} onClick={handleNav}>Projects</Link></li>
+          <li className='p-2'><Link to="Experience" smooth={true} duration={500} onClick={handleNav}>Experience</Link></li>
+          <li className='p-2'><Link to="Contact" smooth={true} duration={500} onClick={handleNav}>Contact</Link></li>
         </ul>
       </div>
     </div>
