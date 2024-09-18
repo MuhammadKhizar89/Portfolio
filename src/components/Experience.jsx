459d80fafe680e7f303dfa0d7a4cab2db52901d9
MuhammadKhizar89 <muhammadkhizar8919@gmail.com>
2024-09-18 23:19:33 +0500

    Fixed

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -8,9 +8,14 @@ const Experience = () => {
                     My <span>Experience</span>
                 </h2>
                 <div className="mb-[20px] self-center text-white relative bg-gray-800/20 p-4 rounded-3xl max-w-[600px] ">
-                    <p>0 Company</p>
-                    <p className="text-gray-400">(2024June -2024July)</p>
+                    <p>1 Company</p>
+                    <p className="text-gray-400">(2024June -2024August)</p>
                     <p className="text-gray-500">Axtra Software</p>
+                </div>
+                <div className="mb-[20px] self-center text-white relative bg-gray-800/20 p-4 rounded-3xl max-w-[600px] ">
+                    <p>FreeLancer</p>
+                    <p className="text-gray-400">(2024August -2024July)</p>
+                    <p className="text-gray-500">FreeLancer</p>
                 </div>
      {/* <div className='h-[50px] w-[2px] bg-slate-400 my-l mx-auto'></div>
         <div className='mb-[20px] self-center text-white relative bg-gray-800/20 p-4 rounded-3xl max-w-[600px] '>
